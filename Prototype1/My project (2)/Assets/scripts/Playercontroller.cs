a4fb5f4f10dd2563e7d601f683fe1092ffcfe889
wilsonp-beep <wilsonp@roverkids.org>
2026-04-23 13:55:32 -0400

    Player control finished project

---
+++ Prototype1/My project (2)/Assets/scripts/Playercontroller.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour
+{
+    public float speed = 25.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward*Time.deltaTime*speed);
+    }
+}
